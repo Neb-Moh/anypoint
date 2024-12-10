bc212410d76c6396b02a297db3fc0a2e274d36e6
Mohammed <nmohammed@sfgmembers.com>
2024-12-10 11:07:56 -0600

    tesst

--- contract.tf
+++ contract.tf
@@ -15,6 +15,9 @@ provider "anypoint" {
   # client_id = "27058ce4cbc14981b1a40017074f96ad"           # optionally use ANYPOINT_CLIENT_ID env var
   # client_secret = "d55E420552344DAd89A16243983371EC"     # optionally use ANYPOINT_CLIENT_SECRET env var
 
+  client_id = "ad53e49ba60a45df946b010efa3178df"           # optionally use ANYPOINT_CLIENT_ID env var
+  client_secret = "6ebB3245cA994Dc68F0E9126B5698566"     # optionally use ANYPOINT_CLIENT_SECRET env var
+
 }
 
 resource "anypoint_env" "env" {
@@ -23,6 +26,6 @@ resource "anypoint_env" "env" {
   type = "sandbox"              # environment type : sandbox/production
 }
 resource "tfe_team" "test" {
-  name         = "my-team-name"
-  organization = "my-org-name"
+  name         = "Team A"
+  organization = "test"
 }
