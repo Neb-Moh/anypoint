241c5e64875d3dac22aa3103bd721a45afca47d1
Mohammed <nmohammed@sfgmembers.com>
2024-12-10 16:37:56 -0600

    tesst

--- env.tf
+++ env.tf
@@ -5,12 +5,13 @@ resource "anypoint_env" "dev_env" {
   type = "sandbox"              # environment type : sandbox/production
 }
 
-resource "anypoint_env" "prod_env" {
+resource "anypoint_env" "PROD" {
 #   org_id = anypoint_bg.bg.id    # environment related business group
   org_id = var.root_org
   name = "Prod"                  # environment name
   type = "production"              # environment type : sandbox/production
 }
+
 
 resource "anypoint_env" "stg_env" {
 #   org_id = anypoint_bg.bg.id    # environment related business group
