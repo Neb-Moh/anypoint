93aef69b74cccc41fbc70fcda5b13e9fa73d5883
Mohammed <nmohammed@sfgmembers.com>
2024-12-10 10:15:04 -0600

    tesst

--- contract.tf
+++ contract.tf
@@ -9,11 +9,11 @@ terraform {
 
 provider "anypoint" {
 
-  username = "Neb_SFG"
-  password = "hiraH123"
+  # username = "Neb_SFG"
+  # password = "hiraH123"
  
-  #  client_id = "27058ce4cbc14981b1a40017074f96ad"           # optionally use ANYPOINT_CLIENT_ID env var
-  # client_secret = "d55E420552344DAd89A16243983371EC"     # optionally use ANYPOINT_CLIENT_SECRET env var
+  client_id = "27058ce4cbc14981b1a40017074f96ad"           # optionally use ANYPOINT_CLIENT_ID env var
+   client_secret = "d55E420552344DAd89A16243983371EC"     # optionally use ANYPOINT_CLIENT_SECRET env var
 
 }
 
