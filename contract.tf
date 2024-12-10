aa118bcc2d5cc44f10c0c72419d1dc0a15b8760e
Mohammed <nmohammed@sfgmembers.com>
2024-12-10 13:14:00 -0600

    tesst

--- contract.tf
+++ contract.tf
@@ -12,8 +12,8 @@ provider "anypoint" {
   # username = "Neb_SFG"
   # password = "hiraH123"
  
-  client_id = vars.client_id          # optionally use ANYPOINT_CLIENT_ID env var
-  client_secret = vars.client_secret    # optionally use ANYPOINT_CLIENT_SECRET env var
+  client_id = var.client_id          # optionally use ANYPOINT_CLIENT_ID env var
+  client_secret = var.client_secret    # optionally use ANYPOINT_CLIENT_SECRET env var
 
   # client_id = "ad53e49ba60a45df946b010efa3178df"           # optionally use ANYPOINT_CLIENT_ID env var
   # client_secret = "6ebB3245cA994Dc68F0E9126B5698566"     # optionally use ANYPOINT_CLIENT_SECRET env var
