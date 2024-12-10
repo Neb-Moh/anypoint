b2d336ec8f9cec2d77241ba46424c997d4179433
Mohammed <nmohammed@sfgmembers.com>
2024-12-10 14:26:40 -0600

    tesst

--- variables.tf
+++ variables.tf
@@ -1,14 +1,14 @@
-variable "username" {
-  type = string
-  default = "neb_sfg"
+# variable "username" {
+#   type = string
+#   default = "neb_sfg"
 
-}
+# }
 
-variable "password" {
-  type = string
-  default = "hiraH123"
+# variable "password" {
+#   type = string
+#   default = "hiraH123"
 
-}
+# }
 
 
 variable "root_org" {
@@ -16,15 +16,15 @@ variable "root_org" {
   default = "9372156a-34ee-468c-ad7e-84f2f66f389b"
   
 }
-# variable "client_id" {
-#   type = string
-#   default = "ad53e49ba60a45df946b010efa3178df"
-# }
+variable "client_id" {
+  type = string
+  default = "ad53e49ba60a45df946b010efa3178df"
+}
 
-# variable "client_secret" {
-#   type = string
-#   default = "6ebB3245cA994Dc68F0E9126B5698566"
-# }
+variable "client_secret" {
+  type = string
+  default = "6ebB3245cA994Dc68F0E9126B5698566"
+}
 
 # variable "access_token" {
 # }
