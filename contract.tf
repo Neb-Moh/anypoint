db0a8987fda5e8812f1fb3744940da3b650ca363
Mohammed <nmohammed@sfgmembers.com>
2024-12-10 09:28:23 -0600

    tesst

--- contract.tf
+++ contract.tf
@@ -9,16 +9,16 @@ terraform {
 
 provider "anypoint" {
 
-  username = "Neb_SFG"
-  password = "hiraH123"
+  # username = "Neb_SFG"
+  # password = "hiraH123"
  
-  # client_id = "27058ce4cbc14981b1a40017074f96ad"           # optionally use ANYPOINT_CLIENT_ID env var
-  # client_secret = "d55E420552344DAd89A16243983371EC"     # optionally use ANYPOINT_CLIENT_SECRET env var
+   client_id = "27058ce4cbc14981b1a40017074f96ad"           # optionally use ANYPOINT_CLIENT_ID env var
+  client_secret = "d55E420552344DAd89A16243983371EC"     # optionally use ANYPOINT_CLIENT_SECRET env var
 
 }
 
-resource "anypoint_env" "env" {
-  org_id = "9372156a-34ee-468c-ad7e-84f2f66f389b"    # environment related business group
-  name = "DEV"                  # environment name
-  type = "sandbox"              # environment type : sandbox/production
-}
+# resource "anypoint_env" "env" {
+#   org_id = "9372156a-34ee-468c-ad7e-84f2f66f389b"    # environment related business group
+#   name = "DEV"                  # environment name
+#   type = "sandbox"              # environment type : sandbox/production
+# }
